48d3e80aa87d222bb15568c3b38e3124ea8ed5e4
Anna Vozleeva <s19apr2000@yandex.ru>
2022-10-22 13:58:34 +0300

    ver 1

--- catsGame/ViewController.swift
+++ catsGame/ViewController.swift
@@ -4,16 +4,47 @@
 //
 //  Created by Anna on 30.04.2022.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - IBOutlet
+    @IBOutlet weak var circleButton: UIButton!
+    
+    //MARK: - lifecycle funcs
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        creativeCircle()
     }
+   
+    //MARK: - IBAction
+    @IBAction func clickCircle(_ sender: UIButton) {
+        circleButton.frame.origin.y = randomY()
+        circleButton.frame.origin.x = randomX()
+    }
+    
+    //MARK: - flow funcs
 
-
+    private func randomX() -> CGFloat{
+        return CGFloat.random(in: 0..<(view.frame.maxX - circleButton.frame.size.width))
+    }
+    
+    private func randomY() -> CGFloat{
+        return CGFloat.random(in : 0..<(view.frame.maxY - circleButton.frame.size.height))
+    }
+    
+    private func randomColor() -> UIColor {
+        let r = CGFloat(arc4random()) / CGFloat(UInt32.max)
+        let g = CGFloat(arc4random()) / CGFloat(UInt32.max)
+        let b = CGFloat(arc4random()) / CGFloat(UInt32.max)
+        
+        return UIColor(red: r, green: g, blue: b, alpha: 1.0)
+    }
+    
+    private func creativeCircle() {
+        circleButton.frame = CGRect(x: randomX(), y: randomY(), width: view.frame.maxY/8, height: view.frame.maxY/8)
+        circleButton.backgroundColor = randomColor()
+        circleButton.layer.cornerRadius = circleButton.frame.size.width / 2
+    }
 }
 
